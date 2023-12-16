e58f1d4f9f7ebb21c26bf7b90be62b6b3858a4e9
Erik Z <molagg92@gmail.com>
2023-12-16 08:09:26 -0800

    made object within object

--- CapstonePage/Controllers/ServicesController.cs
+++ CapstonePage/Controllers/ServicesController.cs
@@ -13,5 +13,26 @@ namespace Capstone.Controllers
       List<Service> allServices = Service.GetAll();
       return View(allServices);
     }
+    [HttpGet("/services/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/services")]
+    public ActionResult Create(string serviceName)
+    {
+      Service newService = new Service(serviceName);
+      return RedirectToAction("Index");
+    }
+      [HttpGet("/services/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Service selectedService = Service.Find(id);
+      List<Employee> serviceEmployees = selectedService.Employees;
+      model.Add("service", selectedService);
+      model.Add("employees", serviceEmployees);
+      return View(model);
+    }
   }
 }
